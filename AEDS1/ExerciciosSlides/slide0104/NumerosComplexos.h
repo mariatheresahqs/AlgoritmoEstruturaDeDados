1f7976112dda403b80d38d649ff5851ecee264ae
Maria Theresa <mariatheresahenriques@gmail.com>
2019-01-15 22:50:33 -0200

    Update NumerosComplexos.h

--- AEDS1/ExerciciosSlides/slide0104/NumerosComplexos.h
+++ AEDS1/ExerciciosSlides/slide0104/NumerosComplexos.h
@@ -21,37 +21,4 @@ void soma(NumeroComplexo* numero1, NumeroComplexo* numero2);
 void ehReal(NumeroComplexo* numero);
 
 
-void atribui(NumeroComplexo* numero){
-    printf("Digite o valor inteiro do número real:\n");
-    scanf("%d", &(*numero).parteReal);
-    printf("Digite o valor inteiro do número imaginário:\n");
-    scanf("%d", &(*numero).parteImaginaria);
-}
-
-void imprime(NumeroComplexo* numero){
-    printf("%d + %di\n", (*numero).parteReal, (*numero).parteImaginaria);
-}
-
-void copia(NumeroComplexo* numero1,NumeroComplexo* numero2){
-    (*numero1).parteReal = (*numero2).parteReal;
-    (*numero1).parteImaginaria = (*numero2).parteImaginaria;
-    printf("Os valores do segundo número foram copiados para o primeiro. O primeiro"
-           " valor agora é: %d + %di.\n", (*numero1).parteReal, (*numero1).parteImaginaria);
-}
-
-void soma(NumeroComplexo* numero1, NumeroComplexo* numero2){
-    NumeroComplexo soma;
-    soma.parteReal = (*numero1).parteReal + (*numero2).parteReal;
-    soma.parteImaginaria = (*numero1).parteImaginaria + (*numero2).parteImaginaria;
-    printf("O resultado da soma é: %d + %di.\n", soma.parteReal, soma.parteImaginaria);
-}
-
-void ehReal(NumeroComplexo* numero){
-    if((*numero).parteImaginaria==0){
-        printf("Número digitado é um número real.\n");
-    } else{
-        printf("Número digitado não é um número real.\n");
-    }
-}
-
 #endif //SLIDE0104_NUMEROSCOMPLEXOS_H
